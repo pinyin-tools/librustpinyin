7ae1658e76c6d24298be9c2ac7164572049d219b
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2014-10-06 22:27:00 +0800

    re #1_add_comment add comments to read.rs
    re #1_add_comments update comments of read.rs
    
    re #1_add_comment delete useless comments

--- src/bin/read.rs
+++ src/bin/read.rs
@@ -4,12 +4,18 @@ use std::io;
 
 use pinyinengine::pinyin2suggestion;
 use pinyinengine::create_db_from_csv;
+
 fn main() {
 
     let db = create_db_from_csv("../data/filtered_db.csv");
 
+    // Read from stdin line by line.
     for line in io::stdin().lines() {
+
         let string = line.unwrap();
+
+        // Search suggestion(s) (i.e chinese words) for the given pinyin and display
+        // each of them.
         for suggestion in pinyin2suggestion(&db, string.as_slice()).iter() {
             println!("{} ", suggestion);
         }
